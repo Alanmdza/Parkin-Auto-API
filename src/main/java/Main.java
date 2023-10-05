70a1f310d9b6cb8ea94264d79479a7e24491fbdc
Alanmdza <alanlm4r@gmail.com>
2023-10-05 13:17:35 -0300

    gui terminada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,37 @@
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+
 import org.json.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
-import spark.Spark;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) throws JsonMappingException, JsonProcessingException, JSONException {
         JSONObject jsonObject = new JSONObject(performHttpGet("http://localhost:8180"));
         JSONObject lugares = jsonObject.getJSONObject("lugares");
-
+        enableCORS("*", "*", "*");
         Spark.get("/get", (req, res) -> {
             res.type("application/json"); // Cambia el tipo de contenido a JSON
             return lugares;
-            
+
         });
 
+        Spark.get("/monitor", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/files/index.vtl");
+            model.put("title", "Monitorizacion");
+            model.put("css", "templates/files/style.vtl");
+            model.put("js", "templates/files/script.vtl");
+            return new VelocityTemplateEngine().render(new ModelAndView(model, "templates/layout.vtl"));
+        });
 
         Spark.post("/post", (request, response) -> {
             // Obtiene el cuerpo del POST
@@ -27,10 +41,8 @@ public class Main {
             JsonNode jsonNode = objectMapper.readTree(body);
             JSONObject lugar = lugares.getJSONObject(jsonNode.get("lugar").asText());
             lugar.put("patente", jsonNode.get("patente").asText());
-            lugar.put("ocupado", jsonNode.get("ocupado").asText());
+            lugar.put("ocupado", jsonNode.get("ocupado"));
             lugares.put(jsonNode.get("lugar").asText(), lugar);
-            // JsonNode lugarJson = objectMapper.readTree(jsonText);
-            // lugares.put("nombre_del_lugar", lugarJson);
             return "POST recibido con éxito";
         });
 
@@ -58,5 +70,13 @@ public class Main {
         }
 
         return response;
+    }
+
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+        });
     }
 }
